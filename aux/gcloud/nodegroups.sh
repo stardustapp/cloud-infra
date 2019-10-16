e513c586925fbb8ea6a4777ee58bc79269eb1ac0
Daniel Lamando <dan+pixelbook@danopia.net>
2019-10-15 18:01:28 -0700

    aux/gcloud: Refresh nodes; add non-preemptible node

--- aux/gcloud/nodegroups.sh
+++ aux/gcloud/nodegroups.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-gcloud container \
-  node-pools create micro2 \
-  --disk-size 10GB \
-  --enable-autorepair \
-  --enable-autoupgrade \
-  --image-type COS_CONTAINERD \
-  --machine-type f1-micro \
-  --max-pods-per-node 25 \
-  --num-nodes 2 \
-  --node-labels purpose=app \
-  --enable-autoscaling \
-  --min-nodes 1 \
-  --max-nodes 3
+# gcloud container \
+#   node-pools create micro2 \
+#   --disk-size 10GB \
+#   --enable-autorepair \
+#   --enable-autoupgrade \
+#   --image-type COS_CONTAINERD \
+#   --machine-type f1-micro \
+#   --max-pods-per-node 25 \
+#   --num-nodes 2 \
+#   --node-labels purpose=app \
+#   --enable-autoscaling \
+#   --min-nodes 1 \
+#   --max-nodes 3
 
 gcloud container \
-  node-pools create small4 \
+  node-pools create small5 \
   --disk-size 10GB \
   --enable-autorepair \
   --enable-autoupgrade \
@@ -23,11 +23,26 @@ gcloud container \
   --machine-type g1-small \
   --max-pods-per-node 25 \
   --preemptible \
-  --num-nodes 1 \
-  --node-labels purpose=app
+  --enable-autoscaling \
+  --min-nodes 0 \
+  --max-nodes 2 \
+  --node-labels purpose=app,lifetime=preemptible \
+  --node-taints cloud.google.com/gke-preemptible="true":PreferNoSchedule
 
 gcloud container \
-  node-pools create build1 \
+  node-pools create persist1 \
+  --disk-size 15GB \
+  --enable-autorepair \
+  --enable-autoupgrade \
+  --image-type COS_CONTAINERD \
+  --machine-type g1-small \
+  --max-pods-per-node 25 \
+  --num-nodes 1 \
+  --node-labels purpose=app,lifetime=persistent \
+  --node-taints cloud.google.com/gke-preemptible="false":PreferNoSchedule
+
+gcloud container \
+  node-pools create build2 \
   --disk-size 25GB \
   --enable-autorepair \
   --enable-autoupgrade \
@@ -35,7 +50,7 @@ gcloud container \
   --machine-type n1-standard-1 \
   --max-pods-per-node 25 \
   --num-nodes 0 \
-  --node-labels purpose=build \
+  --node-labels purpose=build,lifetime=preemptible \
   --node-taints cloud.google.com/gke-preemptible="true":NoSchedule \
   --preemptible \
   --enable-autoscaling \
