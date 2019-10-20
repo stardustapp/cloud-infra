7b9796232b150f715804f1f110a9c0333c3f655b
Daniel Lamando <apt@danopia.net>
2019-10-20 01:58:39 -0700

    external-dns: Start internal DNS concept

--- manifests/apply-all.sh
+++ manifests/apply-all.sh
@@ -5,9 +5,10 @@
 kustomize build manifests/datadog-agent | kubectl apply -f -
 kubectl apply -k manifests/dust-domain
 kubectl apply -k manifests/dust-poc
-kubectl apply -k manifests/external-dns
 kustomize build manifests/ingress-internet | kubectl apply -f -
+kubectl apply -k manifests/ingress-internet-clouddns
 kustomize build manifests/ingress-wg69 | kubectl apply -f -
+kubectl apply -k manifests/ingress-wg69-clouddns
 kustomize build manifests/lambdabot | kubectl apply -f -
 kustomize build manifests/nagios | kubectl apply -f -
 kustomize build manifests/reggie | kubectl apply -f -
