8ba77f03e83c70805f9f22c2be39ef442fa95957
Daniel Lamando <apt@danopia.net>
2019-10-19 20:39:51 -0700

    Update manifests/apply-all.sh

--- manifests/apply-all.sh
+++ manifests/apply-all.sh
@@ -1,6 +1,15 @@
 #!/bin/sh -ex
 
-go get github.com/kubernetes-sigs/kustomize/cmd/kustomize
+#go get github.com/kubernetes-sigs/kustomize/cmd/kustomize
 
-kustomize build dust-domain | kubectl apply -f -
-kustomize build ingress-nginx | kubectl apply -f -
+kustomize build manifests/datadog-agent | kubectl apply -f -
+kustomize build manifests/dust-domain | kubectl apply -f -
+kustomize build manifests/dust-poc | kubectl apply -f -
+kustomize build manifests/external-dns | kubectl apply -f -
+kustomize build manifests/ingress-internet | kubectl apply -f -
+kustomize build manifests/ingress-wg69 | kubectl apply -f -
+kustomize build manifests/lambdabot | kubectl apply -f -
+kustomize build manifests/nagios | kubectl apply -f -
+kustomize build manifests/reggie | kubectl apply -f -
+kustomize build manifests/starnotify | kubectl apply -f -
+kustomize build manifests/wg69 | kubectl apply -f -
