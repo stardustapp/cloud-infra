6b1a8b957ff9e4c7b2f3731bb13248df8476e6f8
Daniel Lamando <apt@danopia.net>
2019-10-19 20:17:29 -0700

    cert-manager: Breaking upgrade to v0.11.0

--- manifests/cert-manager/apply.sh
+++ manifests/cert-manager/apply.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -ex
 
-kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v0.10.0/cert-manager.yaml
+kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v0.11.0/cert-manager.yaml
