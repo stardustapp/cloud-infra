a011c9644c40f1410ccf863b0465cfb476585b3f
Daniel Lamando <dan@danopia.net>
2019-11-24 16:35:02 -0800

    manifests hygiene

--- manifests/apply-all.sh
+++ manifests/apply-all.sh
@@ -5,11 +5,11 @@
 kustomize build manifests/datadog-agent | kubectl apply -f -
 kubectl apply -k manifests/dust-domain
 kustomize build manifests/dust-poc | kubectl apply -f -
-#kubectl apply -k manifests/dust-poc # fails on 1.16?
+# kubectl apply -k manifests/dust-poc # fails on 1.16?
 kustomize build manifests/ingress-internet | kubectl apply -f -
 kubectl apply -k manifests/ingress-internet-clouddns
 kustomize build manifests/ingress-wg69 | kubectl apply -f -
-kubectl apply -k manifests/ingress-wg69-clouddns
+# kubectl apply -k manifests/ingress-wg69-clouddns
 kustomize build manifests/lambdabot | kubectl apply -f -
 kustomize build manifests/nagios | kubectl apply -f -
 kustomize build manifests/reggie | kubectl apply -f -
