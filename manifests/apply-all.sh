484c4ac739a69225244401e4f16c99f76acac084
Daniel Lamando <dan@danopia.net>
2019-12-28 21:11:21 -0800

    manifests: Use older kustomize syntax, for kubectl

--- manifests/apply-all.sh
+++ manifests/apply-all.sh
@@ -4,14 +4,14 @@
 
 #kustomize build manifests/datadog-agent | kubectl apply -f -
 kubectl apply -k manifests/dust-domain
-kustomize build manifests/dust-poc | kubectl apply -f -
-# kubectl apply -k manifests/dust-poc # fails on 1.16?
+kubectl apply -k manifests/dust-poc
 kustomize build manifests/ingress-internet | kubectl apply -f -
 kubectl apply -k manifests/ingress-internet-clouddns
 kustomize build manifests/ingress-wg69 | kubectl apply -f -
 # kubectl apply -k manifests/ingress-wg69-clouddns
 kustomize build manifests/lambdabot | kubectl apply -f -
+kubectl apply -k manifests/media
 kustomize build manifests/nagios | kubectl apply -f -
 kustomize build manifests/reggie | kubectl apply -f -
 kustomize build manifests/starnotify | kubectl apply -f -
-kustomize build manifests/wg69 | kubectl apply -f -
+kubectl apply -k manifests/wg69
