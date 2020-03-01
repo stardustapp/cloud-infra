174210f6e1339ffe7a7d726b2f79cb185319d4c2
Daniel Lamando <dan@danopia.net>
2020-03-01 22:12:05 +0100

    aux/gcloud: Update nodegroups for higher spend

--- aux/gcloud/nodegroups.sh
+++ aux/gcloud/nodegroups.sh
@@ -14,14 +14,29 @@
 #   --min-nodes 1 \
 #   --max-nodes 3
 
+# gcloud container \
+#   node-pools create small6 \
+#   --disk-size 20GB \
+#   --enable-autorepair \
+#   --enable-autoupgrade \
+#   --image-type COS_CONTAINERD \
+#   --machine-type g1-small \
+#   --max-pods-per-node 25 \
+#   --preemptible \
+#   --enable-autoscaling \
+#   --min-nodes 0 \
+#   --max-nodes 2 \
+#   --node-labels purpose=app,lifetime=preemptible \
+#   --node-taints cloud.google.com/gke-preemptible="true":PreferNoSchedule
+
 gcloud container \
-  node-pools create small6 \
+  node-pools create volatile1 \
   --disk-size 20GB \
   --enable-autorepair \
   --enable-autoupgrade \
   --image-type COS_CONTAINERD \
-  --machine-type g1-small \
-  --max-pods-per-node 25 \
+  --machine-type n1-standard-1 \
+  --max-pods-per-node 50 \
   --preemptible \
   --enable-autoscaling \
   --min-nodes 0 \
@@ -30,13 +45,13 @@ gcloud container \
   --node-taints cloud.google.com/gke-preemptible="true":PreferNoSchedule
 
 gcloud container \
-  node-pools create persist2 \
+  node-pools create persist3 \
   --disk-size 20GB \
   --enable-autorepair \
   --enable-autoupgrade \
   --image-type COS_CONTAINERD \
-  --machine-type g1-small \
-  --max-pods-per-node 25 \
+  --machine-type n1-standard-1 \
+  --max-pods-per-node 50 \
   --num-nodes 1 \
   --node-labels purpose=app,lifetime=persistent \
   --node-taints cloud.google.com/gke-preemptible="false":PreferNoSchedule
