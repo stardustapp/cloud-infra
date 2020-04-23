917a58c048d1198a1fb8655a4dac9b1c79ee703b
Daniel Lamando <dan@danopia.net>
2020-04-23 21:25:46 +0200

    starnotify: Update slackjack hook to allow urls

--- images/multi-starnotify/src/slackjack.js
+++ images/multi-starnotify/src/slackjack.js
@@ -3,7 +3,7 @@ const { runWorker, shortenUrl, notify } = require('./_lib');
 exports.processMessage = function processMessage(data) {
   console.log('slackjack webhook data:', JSON.stringify(data));
 
-  const {channel, text, username} = data.payload;
+  const {channel, text, username, url} = data.payload;
   if (channel && username && text) {
 
     var linesToSkip = 0;
@@ -11,11 +11,16 @@ exports.processMessage = function processMessage(data) {
       linesToSkip = 1;
     }
 
+    const urlSuffix = url
+      ? `\x0F \x0302\x1F${shortenUrl(url)}\x0F`
+      : '';
+
     notify(channel,
         `[\x0307${username}\x0F] `+
         text.replace(/\r/g, '')
           .split('\n').slice(linesToSkip)
-          .join(' - ').slice(0, 140));
+          .join(' - ').slice(0, 140)+
+        urlSuffix);
   }
 }
 
