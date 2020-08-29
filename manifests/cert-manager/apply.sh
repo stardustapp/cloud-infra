da61e493bc93f620d9215b4aff175a70892db92c
Daniel Lamando <dan@danopia.net>
2020-08-29 10:48:23 +0200

    cert-manager: Update to 0.16.1

--- manifests/cert-manager/apply.sh
+++ manifests/cert-manager/apply.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -ex
 
-kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.14.1/cert-manager.yaml
+kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.16.1/cert-manager.yaml
