aedbeb55080bd275851ef539faa505d8055971e4
Daniel Lamando <dan@danopia.net>
2020-12-20 15:02:41 +0100

    ingress-internet: Delete gcloud-external-dns

--- manifests/apply-all.sh
+++ manifests/apply-all.sh
@@ -3,6 +3,7 @@
 #go get github.com/kubernetes-sigs/kustomize/cmd/kustomize
 
 kubectl apply -k manifests/amazon-pod-identity
+kubectl apply -k manifests/code/code-server
 #kustomize build manifests/datadog-agent | kubectl apply -f -
 kubectl apply -k manifests/drone-builds
 kubectl apply -k manifests/drone-gitea
@@ -10,7 +11,6 @@ kubectl apply -k manifests/dust-poc
 kustomize build manifests/ingress-internet/nginx | kubectl apply -f -
 kubectl apply -k manifests/ingress-internet/dns
 kubectl apply -k manifests/ingress-internet/dns-sync
-kubectl apply -k manifests/ingress-internet/gcloud-clouddns
 kustomize build manifests/ingress-wg69 | kubectl apply -f -
 kubectl apply -k manifests/ingress-wg69-clouddns
 kubectl apply -k manifests/kube-pets/blockdevices
