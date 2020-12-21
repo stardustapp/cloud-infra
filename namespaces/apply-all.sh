d15d5d579658a287ccea6d791fad19efa2b71581
Daniel Lamando <dan@danopia.net>
2020-12-21 22:54:33 +0100

    Rename 'manifests' to 'namespaces'

---
+++ namespaces/apply-all.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -eux
+
+mode=$1
+
+kubectl $mode -k namespaces/code/code-server
+#kustomize build namespaces/datadog-agent | kubectl $mode -f -
+kubectl $mode -k namespaces/default/lambdabot
+kubectl $mode -k namespaces/default/reggie
+kubectl $mode -k namespaces/drone-builds
+kubectl $mode -k namespaces/drone-gitea
+kubectl $mode -k namespaces/dust-poc
+kustomize build namespaces/ingress-internet/nginx | kubectl $mode -f -
+kubectl $mode -k namespaces/ingress-internet/dns
+kubectl $mode -k namespaces/ingress-internet/dns-sync
+kustomize build namespaces/ingress-wg69/nginx | kubectl $mode -f -
+kubectl $mode -k namespaces/ingress-wg69/dns
+kubectl $mode -k namespaces/ingress-wg69/dns-sync
+kubectl $mode -k namespaces/kube-pets/blockdevices
+kubectl $mode -k namespaces/kube-system/amazon-pod-identity
+kubectl $mode -k namespaces/media
+kustomize build namespaces/nagios | kubectl $mode -f -
+kubectl $mode -k namespaces/skychat
+kustomize build namespaces/starnotify | kubectl $mode -f -
+kubectl $mode -k namespaces/wg69
