86e441891b8021e51e8f95d098894e8942f47d90
Daniel Lamando <dan@danopia.net>
2021-07-01 22:47:00 +0200

    Remove libera.chat special casing - new default

--- images/multi-starnotify/src/_lib.js
+++ images/multi-starnotify/src/_lib.js
@@ -1,14 +1,7 @@
-const { spawn } = require('child_process');
 const querystring = require('querystring');
 
 const fetch = require('node-fetch');
 const AWS = require('aws-sdk');
-
-// Allow injecting 'starnotify' options e.g. server URL
-const process = require('process');
-const {STARNOTIFY_FLAGS} = process.env;
-const extraFlags = STARNOTIFY_FLAGS ? STARNOTIFY_FLAGS.split(' ') : [];
-console.log('Using extra starnotify opts:', extraFlags);
 
 const sqs = new AWS.SQS();
 const s3 = new AWS.S3();
@@ -20,53 +13,23 @@ exports.sleep = (ms) => new Promise(ok => setTimeout(ok, ms));
 exports.notify = async function notify(channel, message) {
   console.log('Sending to', channel, '-', message);
 
-  // Certain channels should use the new bot
-  if ([
-    '##danopia',
-    '#dagd', '#dagd-ops',
-    '#relrodtest',
-    '#robigalia',
-    '#fedora-aaa-activity',
-    '#hledger', '#hledger-bots',
-    '#stardust-test', '#stardust-noise',
-  ].includes(channel)) {
-    let network = 'libera';
-    if (channel == '#robigalia') network = 'oftc';
-    const payload = JSON.stringify({ "ver": 1,
-      "protocol": "irc", "network": network,
-      "target": channel, "message": message,
-    });
+  let network = 'libera';
+  if (channel == '#robigalia') network = 'oftc';
 
-    const {SQS_QUEUE_URL_BASE, SQS_QUEUE_IRC_OUTBOUND} = process.env;
-    await sqs.sendMessage({
-      QueueUrl: SQS_QUEUE_URL_BASE + SQS_QUEUE_IRC_OUTBOUND,
-      MessageBody: payload,
-      MessageDeduplicationId: Math.random().toString().slice(2),
-      MessageGroupId: ['irc', 'freenode', channel].map(encodeURIComponent).join('/')
-    }).promise();
-
-    return;
-  }
-
-  const app = spawn('starnotify', [
-    ...extraFlags,
-    '-irc-channel', channel,
-    '-message', message || '',
-  ]);
-  app.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`);
-  });
-  app.stderr.on('data', (data) => {
-    console.log(`stderr: ${data}`);
+  const payload = JSON.stringify({ "ver": 1,
+    "protocol": "irc", "network": network,
+    "target": channel, "message": message,
   });
 
-  const code = await new Promise(ok => {
-    app.on('close', ok);
-  });
+  const {SQS_QUEUE_URL_BASE, SQS_QUEUE_IRC_OUTBOUND} = process.env;
+  await sqs.sendMessage({
+    QueueUrl: SQS_QUEUE_URL_BASE + SQS_QUEUE_IRC_OUTBOUND,
+    MessageBody: payload,
+    MessageDeduplicationId: Math.random().toString().slice(2),
+    MessageGroupId: ['irc', 'freenode', channel].map(encodeURIComponent).join('/')
+  }).promise();
 
-  console.log(`child process exited with code ${code}!`);
-  if (code !== 0) throw new Error(
-    `'starnotify' process exited with code ${code}`);
+  return;
 };
 
 exports.storeSpeciman = async function storeSpeciman(key, body) {
