86a875f91536ed7c70d8aa3dcc21445a71713bf8
Daniel Lamando <dan@danopia.net>
2021-11-21 18:36:00 +0100

    Prep more stuff for new cluster

--- namespaces/apply-all.sh
+++ namespaces/apply-all.sh
@@ -6,9 +6,9 @@ mode=$1
 # kubectl $mode -k namespaces/default/lambdabot
 kubectl $mode -k namespaces/default/reggie
 # kubectl $mode -k namespaces/dust-poc
-# kubectl $mode -k namespaces/ingress-internet/nginx
+kubectl $mode -k namespaces/ingress-internet/nginx
 # kubectl $mode -k namespaces/ingress-internet/dns
-# kubectl $mode -k namespaces/ingress-internet/dns-sync
+kubectl $mode -k namespaces/ingress-internet/dns-sync
 # kubectl $mode -k namespaces/ingress-wg69/nginx
 # kubectl $mode -k namespaces/ingress-wg69/dns
 # kubectl $mode -k namespaces/ingress-wg69/dns-sync
@@ -18,7 +18,8 @@ kubectl $mode -k namespaces/kube-pets
 kubectl $mode -k namespaces/monitoring
 # kubectl $mode -k namespaces/nagios
 kubectl $mode -k namespaces/skychat
-# kubectl $mode -k namespaces/starnotify
+kubectl $mode -k namespaces/starnotify
 # kubectl $mode -k namespaces/wg69
 
 # kubectl $mode -f "https://kubernetes-aws-identity.deno.dev/webhook-config.yaml"
+kubectl $mode -f https://raw.githubusercontent.com/kubernetes-sigs/external-dns/master/docs/contributing/crd-source/crd-manifest.yaml
--- namespaces/cert-manager/apply.sh
+++ namespaces/cert-manager/apply.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -ex
 
-kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.1.0/cert-manager.yaml
+kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.6.1/cert-manager.yaml
+kubectl apply -f dns-sync-issuer.yaml
