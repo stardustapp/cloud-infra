1a4158ab2ffa2cba61613650530514195ba953c7
Daniel Lamando <dan@danopia.net>
2021-11-28 21:12:05 +0100

    Enable nagios, update nagios accessory images

--- namespaces/apply-all.sh
+++ namespaces/apply-all.sh
@@ -7,16 +7,14 @@ mode=$1
 kubectl $mode -k namespaces/default/reggie
 # kubectl $mode -k namespaces/dust-poc
 kubectl $mode -k namespaces/ingress-internet/nginx
-# kubectl $mode -k namespaces/ingress-internet/dns
 kubectl $mode -k namespaces/ingress-internet/dns-sync
 # kubectl $mode -k namespaces/ingress-wg69/nginx
-# kubectl $mode -k namespaces/ingress-wg69/dns
 # kubectl $mode -k namespaces/ingress-wg69/dns-sync
 kubectl $mode -k namespaces/kube-pets
 # kubectl $mode -k namespaces/kube-system/amazon-pod-identity
 # kubectl $mode -k namespaces/media
 kubectl $mode -k namespaces/monitoring
-# kubectl $mode -k namespaces/nagios
+kubectl $mode -k namespaces/nagios
 kubectl $mode -k namespaces/skychat
 kubectl $mode -k namespaces/starnotify
 # kubectl $mode -k namespaces/wg69
